f795cf30538624e146f2e1b257dbfdce467a638e
Abbas <mamedovan@yandex.ru>
2024-02-11 18:31:05 +0300

    v1.0.2

--- maklerLanding/src/common/api/configs/botConfigs.js
+++
@@ -1,4 +0,0 @@
-export const BOT_CONFIGS = {
-    token:'6459432648:AAFZkFTMnBQ-pojE8db7FaX9Q0lPsGeydbU',
-    chat_id:'-1001813179310'
-}
--- maklerLanding/src/layout/Form/enum/formFieldsNames.js
+++
@@ -1,7 +0,0 @@
-export const FORM_FIELDS_NAMES = {
-  name: "name",
-  secondName:'secondName',
-  lastName:'lastName',
-  email:'email',
-  phoneNumber:'phoneNumber'
-};
